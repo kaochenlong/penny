679d9bff5d2667583f354c3e33fba94be6903fcc
Eddie Kao <eddie@5xcampus.com>
2024-04-23 16:53:43 +0800

    add new nanny page

--- pet_nanny/urls.py
+++ pet_nanny/urls.py
@@ -3,6 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path("", include("pages.urls")),
-    path("nannies", include("nannies.urls")),
+    path("nannies/", include("nannies.urls")),
     path("admin/", admin.site.urls),
 ]
