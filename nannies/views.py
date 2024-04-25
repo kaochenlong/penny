bcb5e1ec986357f89b89a69544ad1352b5335352
Eddie Kao <eddie@5xcampus.com>
2024-04-25 15:24:48 +0800

    create Nanny

--- nannies/views.py
+++ nannies/views.py
@@ -1,16 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from django.urls import reverse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.decorators.http import require_POST
+from .models import Nanny
 
 
 def index(request):
-    return render(request, "nannies/index.html")
+    nannies = Nanny.objects.all()
+    return render(request, "nannies/index.html", {"nannies": nannies})
 
 
 def new(request):
     return render(request, "nannies/new.html")
 
 
+@require_POST
 def create(request):
-    return HttpResponse("hi")
+    nanny = Nanny(
+        name=request.POST["name"],
+        gender=request.POST["gender"],
+        tel=request.POST["tel"],
+        nickname=request.POST["nickname"],
+        description=request.POST["description"],
+    )
+    nanny.save()
+
+    return redirect(reverse("root"))
