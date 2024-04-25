dbed7f9d507b4c2edde1bc678e8649365fd241d7
Eddie Kao <eddie@5xcampus.com>
2024-04-25 16:07:03 +0800

    R for Nanny

--- nannies/urls.py
+++ nannies/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("new", views.new, name="new"),
     path("add", views.create, name="add"),
+    path("<id>", views.show, name="show"),
 ]
--- nannies/views.py
+++ nannies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views.decorators.http import require_POST
@@ -8,6 +8,11 @@ from .models import Nanny
 def index(request):
     nannies = Nanny.objects.all()
     return render(request, "nannies/index.html", {"nannies": nannies})
+
+
+def show(request, id):
+    nanny = get_object_or_404(Nanny, pk=id)
+    return render(request, "nannies/show.html", {"nanny": nanny})
 
 
 def new(request):
