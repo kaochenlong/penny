2b919bdb598a6e659dc3c0e33e5596f175c5af7c
Eddie Kao <eddie@5xcampus.com>
2024-04-29 15:33:56 +0800

    add flash message

--- nannies/views.py
+++ nannies/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.views.decorators.http import require_POST
 from .models import Nanny
 from .forms import NannyForm
+from django.contrib import messages
 
 
 def index(request):
@@ -17,6 +18,7 @@ def show(request, id):
 
         if form.is_valid():
             form.save()
+            messages.success(request, "更新成功")
 
         return redirect("nannies:show", id=nanny.id)
     else:
@@ -42,6 +44,7 @@ def create(request):
 
     if form.is_valid():
         form.save()
+        messages.success(request, "新增成功")
 
     return redirect("nannies:index")
 
@@ -50,4 +53,5 @@ def create(request):
 def delete(request, id):
     nanny = get_object_or_404(Nanny, pk=id)
     nanny.delete()
+    messages.error(request, "資料已刪除！")
     return redirect("nannies:index")
