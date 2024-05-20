971d69c013949a9f336f485626026bf7302681a2
krystal <krystal93002232@gmail.com>
2024-05-21 02:32:00 +0800

    modify: chroe Sortable & Alpine & htmx to patch position

--- src/scripts/components/drag.js
+++ src/scripts/components/drag.js
@@ -1,41 +1,26 @@
-import { Sortable } from "@shopify/draggable";
-import axios from "axios";
-// import htmx from "htmx.org";
+import Sortable from "sortablejs";
+import Alpine from "alpinejs";
+import htmx from "htmx.org";
 
-const sortable = new Sortable(document.querySelectorAll("#comments"), {
-  draggable: ".comment-item",
-});
+Alpine.data("sortList", () => ({
+  init() {
+    const rootElement = this.$el;
+    const sortable = Sortable.create(rootElement, {
+      animation: 150,
 
-sortable.on("sortable:stop", (e) => {
-  const draggedElement = e.dragEvent.source;
-  const { id, csrf } = draggedElement.dataset;
-
-  if (e.oldIndex != e.newIndex) {
-    axios.defaults.headers.common["X-CSRFToken"] = csrf;
-    axios
-      .post(`/api/v1/comments/${id}/position`, {
-        newIndex: e.newIndex,
-      })
-      .then((response) => {
-        console.log("成功");
-      })
-      .catch((error) => {
-        console.error("Error deleting comment:", error);
-      });
-
-    // htmx.ajax("POST", `/api/v1/comments/${id}/position`, {
-    //   values: {
-    //     newIndex: newIndex,
-    //   },
-    //   headers: {
-    //     "X-CSRFToken": csrf,
-    //   },
-    //   success: function (responseText) {
-    //     console.log("位置更新成功");
-    //   },
-    //   error: function (error) {
-    //     console.error("更新位置時發生錯誤:", error);
-    //   },
-    // });
-  }
-});
+      onEnd: function (evt) {
+        var itemElement = evt.item;
+        const { id, csrf } = itemElement.dataset;
+        htmx.ajax("PATCH", `/api/v1/comments/${id}/position`, {
+          values: {
+            newIndex: evt.newIndex,
+          },
+          headers: {
+            "X-CSRFToken": csrf,
+          },
+          swap: "none",
+        });
+      },
+    });
+  },
+}));
